5c5d768f3110f07be21cbf5de9f046522aa8c56a
Christian Barbosa <christian.barbosa05222001@gmail.com>
2023-09-19 14:26:36 +0800

    Initial Commit

---
+++ facebook/src/main/java/com/chanbarbosa/facebook/Facebook.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.chanbarbosa.facebook;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Facebook {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
